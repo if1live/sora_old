f2f7eb99459a8da903736e6f7c90514aa5060d9a
if1live <4ipperz@gmail.com>
2011-08-13 04:51:46 +0900

    malloc/free based free list(maybe used in operator new/delete)

---
+++ soraTest/karenTest/KRFreeListBlockPoolTest.cpp
@@ -0,0 +1,33 @@
+﻿// Ŭnicode please 
+#include "../SoraTestStdAfx.h"
+#include "karen/KRFreeListBlockPool.h"
+
+using namespace karen;
+
+class KarenBlockTestObj {
+public:
+	KarenBlockTestObj() : a(1) { cnt++; }
+	~KarenBlockTestObj() { cnt--;}
+public:
+	int a;
+	static int cnt;
+};
+int KarenBlockTestObj::cnt = 0;
+
+TEST(KRFreeListBlockPool, test)
+{
+	FreeListBlockPool<sizeof(KarenBlockTestObj)> pool(3);
+	EXPECT_EQ(0, KarenBlockTestObj::cnt);	//constructor is not called
+	void *ptr1 = pool.malloc();
+	void *ptr2 = pool.malloc();
+	void *ptr3 = pool.malloc();
+	//KarenTestObj *ptr4 = pool.malloc();	//dead point (too may request)
+
+	pool.free(ptr2);
+	pool.free(ptr3);
+
+	void *ptr4 = pool.malloc();
+	EXPECT_EQ(ptr4, ptr3);	//free list management is stack based;
+	void *ptr5 = pool.malloc();
+	EXPECT_EQ(ptr5, ptr2);
+}
