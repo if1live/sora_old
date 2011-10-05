1bd2cf9e95634e20ecce2b5fbbe825d32664d857
if1live <4ipperz@gmail.com>
2011-10-05 22:42:10 +0900

    뱀 몸통색 강제 보정하는 쉐이더 추가

--- nadeko/board_view.cc
+++ nadeko/board_view.cc
@@ -82,7 +82,12 @@ tile_size_(tile_size) {
       "{  "
       "vec4 tmp = texture2D(s_texture, v_texcoord); "
       "vec4 tmp1 = vec4(tmp.x * v_color.x, tmp.y * v_color.y, tmp.z * v_color.z, tmp.w * v_color.w);"
-      "gl_FragColor = tmp1;  "
+      "float minimum = 0.3;"
+      "float r = tmp1.x > minimum ? tmp1.x : minimum; "
+      "float g = tmp1.y > minimum ? tmp1.y : minimum; "
+      "float b = tmp1.z > minimum ? tmp1.z : minimum; "
+      "float a = tmp1.w > minimum ? tmp1.w : minimum; "
+      "gl_FragColor = vec4(r, g, b, a);  "
       "}  ";
   body_shader_prog.reset(new runa::ShaderProgram(vertex_src, fragment_src));
 }
