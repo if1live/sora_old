caa5e96aeda3ef89ed7ccf89c4a07e2fa230280b
if1live <4ipperz@gmail.com>
2012-03-16 18:30:38 +0900

    iphone cpp compile

---
+++ yuriko/yuriko/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  yuriko
+//
+//  Created by 유 병후 on 12. 3. 16..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
