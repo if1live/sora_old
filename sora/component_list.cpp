8eb5cf750507eeb97a51e1f8a6ed0f32a3e1ee03
if1live <4ipperz@gmail.com>
2012-03-19 22:10:37 +0900

    add old component based entity system

---
+++ sora/component_list.cpp
@@ -0,0 +1,67 @@
+﻿/*  Copyright (C) 2011 by if1live */
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// Ŭnicode please
+#include "sora_stdafx.h"
+#include "component_list.h"
+#include "component.h"
+#include "template_lib.h"
+
+namespace sora {;
+ComponentList::ComponentList() {
+}
+ComponentList::~ComponentList() {
+  SR_ASSERT(comp_list_.size() == 0);
+  sora::DestroyList(&comp_list_);  
+}
+bool ComponentList::IsExist(Component *comp) const {
+  ConstIterator found = std::find(comp_list_.begin(), comp_list_.end(), comp);
+  if (found == comp_list_.end()) {
+    return false;
+  } else {
+    return true;
+  }
+}
+bool ComponentList::Add(Component *comp) {
+  if (comp == NULL) {
+    return false;
+  }
+  // 넣는것은 맨 앞에 넣자. 왜냐하면 보통 나중에 넣은 컴포넌트가 빠질 확률이 높으니까
+  // 중복삽입을 막기 위해서 검사를 하자
+  Iterator found = std::find(comp_list_.begin(), comp_list_.end(), comp);
+  if (found == comp_list_.end()) {
+    comp_list_.push_front(comp);
+    return true;
+  } else {
+    SR_ASSERT(!"alreay added");
+    return false;
+  }
+}
+bool ComponentList::Remove(Component *comp) {
+  // 나중에 넣은 컴포넌트일수록 사라질 확률이 높으니까 앞부터 검색
+  using std::find;
+  Iterator found = find(comp_list_.begin(), comp_list_.end(), comp);
+  if (found == comp_list_.end()) {
+    return false;
+  } else {
+    comp_list_.erase(found);
+    return true;
+  }
+}
+}
---
+++ sora/component_list.h
@@ -0,0 +1,52 @@
+﻿/*  Copyright (C) 2011 by if1live */
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// Ŭnicode please
+#ifndef SORA_COMPONENT_LIST_H_
+#define SORA_COMPONENT_LIST_H_
+
+namespace sora {;
+class Component;
+
+class ComponentList {
+public:
+  // 리스트를 쓴것은 넣고뺴고가 자주 발생해서이다
+  typedef std::list<Component*> ListType;
+  typedef ListType::iterator Iterator;
+  typedef ListType::const_iterator ConstIterator;
+
+public:
+  ComponentList();
+  ~ComponentList();
+
+  bool Add(Component *comp);
+  bool Remove(Component *comp);
+  bool IsExist(Component *comp) const;
+  int Count() const { return comp_list_.size(); }
+
+  Iterator Begin() { return comp_list_.begin(); }
+  Iterator End() { return comp_list_.end(); }
+  ConstIterator Begin() const { return comp_list_.begin(); }
+  ConstIterator End() const { return comp_list_.end(); }
+private:
+  ListType comp_list_;
+};
+}
+
+#endif  // SORA_COMPONENT_LIST_H_
