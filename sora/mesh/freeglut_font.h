8700f4ebdfc6329910458dd52d56e5566fa00cec
if1live <4ipperz@gmail.com>
2012-04-22 23:46:57 +0900

    remove freeglut bitmap font

--- sora/mesh/freeglut_font.h
+++ sora/mesh/freeglut_font.h
@@ -39,26 +39,7 @@ namespace sora {;
 //#if defined(_MSC_VER) || defined(__CYGWIN__) || defined(__MINGW32__) || defined(__WATCOMC__)
 #   define  GLUT_STROKE_ROMAN               ((void *)0x0000)
 #   define  GLUT_STROKE_MONO_ROMAN          ((void *)0x0001)
-#   define  GLUT_BITMAP_9_BY_15             ((void *)0x0002)
-#   define  GLUT_BITMAP_8_BY_13             ((void *)0x0003)
-#   define  GLUT_BITMAP_TIMES_ROMAN_10      ((void *)0x0004)
-#   define  GLUT_BITMAP_TIMES_ROMAN_24      ((void *)0x0005)
-#   define  GLUT_BITMAP_HELVETICA_10        ((void *)0x0006)
-#   define  GLUT_BITMAP_HELVETICA_12        ((void *)0x0007)
-#   define  GLUT_BITMAP_HELVETICA_18        ((void *)0x0008)
 //#else
-
-/* The bitmap font structure */
-typedef struct tagSFG_Font SFG_Font;
-struct tagSFG_Font
-{
-  char*           Name;         /* The source font name             */
-  int             Quantity;     /* Number of chars in font          */
-  int             Height;       /* Height of the characters         */
-  const GLubyte** Characters;   /* The characters mapping           */
-
-  float           xorig, yorig; /* Relative origin of the character */
-};
 
 
 /* The stroke font structures */
@@ -97,12 +78,6 @@ struct tagSFG_StrokeFont
 /*
  * Font stuff, see freeglut_font.c
  */
-//void    glutBitmapCharacter( void* font, int character );
-//int     glutBitmapWidth( void* font, int character );
-//void    glutBitmapString( void* font, const unsigned char *string );
-//int     glutBitmapLength( void* font, const unsigned char* string );
-//int     glutBitmapHeight( void* font );
-
 int     glutStrokeWidth( void* font, int character );
 int     glutStrokeLength( void* font, const unsigned char* string );
 GLfloat glutStrokeHeight( void* font );
